0fdc250f95e80fcfe0fce22f69610c47ea271e31
BoBRoID <n.gilko@gmail.com>
2017-05-28 23:28:21 +0300

    added and updated models

--- models/Books.php
+++ models/Books.php
@@ -9,10 +9,16 @@ use Yii;
  *
  * @property int $id
  * @property string $slug
+ * @property string $language_code
+ * @property string $title
+ * @property string $description
+ * @property string $download_link
+ * @property int $views
  *
  * @property BooksCategories[] $booksCategories
  * @property BooksLinks[] $booksLinks
  * @property BooksPhotos[] $booksPhotos
+ * @property BooksViews[] $booksViews
  */
 class Books extends \yii\db\ActiveRecord
 {
@@ -30,8 +36,11 @@ class Books extends \yii\db\ActiveRecord
     public function rules()
     {
         return [
-            [['slug'], 'required'],
+            [['slug', 'language_code'], 'required'],
+            [['title', 'description', 'download_link'], 'string'],
+            [['views'], 'integer'],
             [['slug'], 'string', 'max' => 255],
+            [['language_code'], 'string', 'max' => 2],
         ];
     }
 
@@ -43,6 +52,11 @@ class Books extends \yii\db\ActiveRecord
         return [
             'id' => 'ID',
             'slug' => 'Slug',
+            'language_code' => 'Language Code',
+            'title' => 'Title',
+            'description' => 'Description',
+            'download_link' => 'Download Link',
+            'views' => 'Views',
         ];
     }
 
@@ -68,5 +82,13 @@ class Books extends \yii\db\ActiveRecord
     public function getBooksPhotos()
     {
         return $this->hasMany(BooksPhotos::className(), ['book_id' => 'id']);
+    }
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getBooksViews()
+    {
+        return $this->hasMany(BooksViews::className(), ['book_id' => 'id']);
     }
 }
---
+++ models/forms/BookForm.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: bobroid
+ * Date: 5/28/17
+ * Time: 11:09 PM
+ */
+
+namespace app\models\forms;
+
+
+use yii\base\Model;
+
+class BookForm extends Model
+{
+
+    public $id;
+
+    public $slug;
+
+    public $
+
+}
