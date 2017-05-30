c1a442e894394a1e15be867432beffe45f8b22fc
BoBRoID <n.gilko@gmail.com>
2017-05-30 22:55:43 +0300

    added some models

---
+++ models/BooksAuthors.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "books_authors".
+ *
+ * @property int $id
+ * @property string $name
+ *
+ * @property BooksAuthorsRef[] $booksAuthorsRefs
+ */
+class BooksAuthors extends \yii\db\ActiveRecord
+{
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return 'books_authors';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['name'], 'string'],
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'name' => 'Name',
+        ];
+    }
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getBooksAuthorsRefs()
+    {
+        return $this->hasMany(BooksAuthorsRef::className(), ['book_author_id' => 'id']);
+    }
+}
