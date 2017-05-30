8e65eb41e85863a7e528461bf8d8b69afdd730f6
Mihailmihail111 <raven29_89@mail.ru>
2017-05-31 00:30:12 +0300

    added adaptive menu

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -31,6 +31,12 @@ AppAsset::register($this);
     <div class="navbar navbar-inverse navbar-fixed-top" role="banner">
         <div class="container">
             <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                    <span class="sr-only">Toggle navigation</span> 
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span> 
+                </button>
                <?php
 //                   NavBar::begin([
 //                       'brandLabel' =>  Yii::$app->name ,
