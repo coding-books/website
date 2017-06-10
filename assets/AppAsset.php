eeef14000e65624c202ab2bcaa29552b35fc8312
Kravalg <kravtsov.yar@gmail.com>
2017-06-10 14:12:52 +0300

    fixed bugs with layout

--- assets/AppAsset.php
+++ assets/AppAsset.php
@@ -18,7 +18,7 @@ class AppAsset extends AssetBundle
     public $basePath = '@webroot';
     public $baseUrl = '@web';
     public $css = [
-        'css/main.css?14',
+        'css/main.css?15',
         'css/prettyPhoto.css',
         'css/responsive.css',
     ];
