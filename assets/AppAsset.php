c3f0d71d2c3adf38fa39095935b40385607a635f
Kravalg <kravtsov.yar@gmail.com>
2017-06-21 11:32:39 +0300

    fixed mobile layout [books posters]

--- assets/AppAsset.php
+++ assets/AppAsset.php
@@ -18,7 +18,7 @@ class AppAsset extends AssetBundle
     public $basePath = '@webroot';
     public $baseUrl = '@web';
     public $css = [
-        'css/main.css?28',
+        'css/main.css?29',
         'css/prettyPhoto.css',
         'css/responsive.css?1',
     ];
