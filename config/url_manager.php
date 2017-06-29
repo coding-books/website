4d3312cf0a3081350a82da0e8e1effbd24644ec5
Kravalg <kravtsov.yar@gmail.com>
2017-06-30 01:52:48 +0300

    fixed bug with multi language login [9]

--- config/url_manager.php
+++ config/url_manager.php
@@ -4,6 +4,7 @@ $urlLangs = require(__DIR__ . '/url_lang.php');
 return [
     'class' => 'codemix\localeurls\UrlManager',
     'languages' => $urlLangs,
+    'enableDefaultLanguageUrlCode' => true,
     'enableLanguagePersistence' => false,
     'enablePrettyUrl' => true,
     'showScriptName' => false,
