5c2a685b42e0a07bd47b6299389c2ec0698884d6
Kravalg <kravtsov.yar@gmail.com>
2017-07-01 03:50:37 +0300

    added social accounts

--- assets/AppAsset.php
+++ assets/AppAsset.php
@@ -18,7 +18,7 @@ class AppAsset extends AssetBundle
     public $basePath = '@webroot';
     public $baseUrl = '@web';
     public $css = [
-        'css/main.css?36',
+        'css/main.css?37',
         'css/prettyPhoto.css',
         'css/responsive.css?1',
     ];
