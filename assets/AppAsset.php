4a1e31975dfa24fafa118cc71c24316da496f78f
Kravalg <kravtsov.yar@gmail.com>
2017-07-03 19:58:05 +0300

    fixed bug with mobile layout

--- assets/AppAsset.php
+++ assets/AppAsset.php
@@ -18,7 +18,7 @@ class AppAsset extends AssetBundle
     public $basePath = '@webroot';
     public $baseUrl = '@web';
     public $css = [
-        'css/main.css?38',
+        'css/main.css?39',
         'css/prettyPhoto.css',
         'css/responsive.css?1',
     ];
