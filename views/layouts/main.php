a171817f3a67358d15c4c532a62164f0fb81f014
Krava <Kravalg@users.noreply.github.com>
2017-07-23 17:58:45 +0300

    added google analytics counter

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -63,6 +63,15 @@ $this->registerJs($js);
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <?= Html::csrfMetaTags() ?>
     <title><?= Html::encode($this->title) ?></title>
+    <script>
+          (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+          (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+          m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+          })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+          ga('create', 'UA-101523366-1', 'auto');
+          ga('send', 'pageview');
+    </script>
     <?php $this->head() ?>
 </head>
 <body>
