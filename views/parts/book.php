4a46807eac4acdf2fd90b76b07299e0b6cb9f745
Kravalg <kravtsov.yar@gmail.com>
2017-10-31 19:08:53 +0200

    removed books caching

--- views/parts/book.php
+++ views/parts/book.php
@@ -7,10 +7,8 @@ use yii\helpers\Url;
 /* @var $model \app\models\Books */
 
 $this->title = Yii::t('seo','{appName} - Coding books | Programming Library', ['appName' => \Yii::$app->name]);
-$this->params['model'] = $model;
 ?>
 <div class="col-md-4 col-sm-6 col-xs-12">
-    <?php if ($this->beginCache('BookView' . $model->id, ['duration' => 86400])) {  ?>
         <div class="single-blog row">
         <div class="col-md-6 col-sm-6 col-xs-12">
             <a href="<?= Url::to(['book/view', 'slug' => $model->slug, 'id' => $model->id]) ?>">
@@ -23,39 +21,36 @@ $this->params['model'] = $model;
                     <h4>
                         <?= Html::encode($model->title) ?>
                         <span class="badge" title="<?= Yii::t('books','The language of the book') ?>">
-                                    <?= Html::encode(strtoupper($model->language_code)) ?>
-                                </span>
+                            <?= Html::encode(strtoupper($model->language_code)) ?>
+                        </span>
                     </h4>
                 </a>
                 <div class="button-actions">
                     <a href="<?= Url::to(['book/view', 'slug' => $model->slug, 'id' => $model->id]) ?>" class="btn btn-xs btn-info">
-                        <?= $this->renderDynamic('return Yii::t("books","Read More");'); ?>
+                        <?= Yii::t("books","Read More"); ?>
                     </a>
                     <a href="<?= Url::to($model->getDownloadLink()) ?>" class="btn btn-xs btn-success" target="_blank">
-                        <?= $this->renderDynamic('return Yii::t("books", "Download");') ?>
+                        <?= Yii::t("books", "Download") ?>
                     </a>
                     <a href="<?= Url::to([
                         '/pdfjs',
                         'file' => $model->getDownloadLink() . '#page=' . $model->getLastPage(),
                         'zoom' => 'page-width'
                     ]) ?>" class="btn btn-xs btn-default">
-                        <?= $this->renderDynamic('return Yii::t("books", "Read online");') ?>
+                        <?= Yii::t("books", "Read online"); ?>
                     </a>
                 </div>
                 <ul class="post-meta">
                     <li>
                         <i class="fa fa-clock-o"></i>
                         <strong>
-                            <?= $this->renderDynamic('return Yii::t("books", "Posted On");') ?>
+                            <?= Yii::t("books", "Posted On") ?>
                         </strong>
                         <br>
-                        <?= $this->renderDynamic('return \Yii::$app->formatter->asDate($this->params["model"]->created, "medium");') ?>
+                        <?= Yii::$app->formatter->asDate($model->created, "medium") ?>
                     </li>
                 </ul>
             </div>
         </div>
     </div>
-    <?php
-        $this->endCache();}
-    ?>
 </div>
